70023309bb0ad49ed5b9990798df2d29359ff7c2
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-05-27 14:31:54 +0600

    --
    --

--- src/main/java/app/common/repo/UnitOfMeasureRepo.java
+++ src/main/java/app/common/repo/UnitOfMeasureRepo.java
@@ -15,6 +15,6 @@ public interface UnitOfMeasureRepo extends JpaRepository<UnitOfMeasure,Long> {
 
     @Query("select x.productCatIds from UnitOfMeasure x where x.name=:name and x.orgId=:orgId ")
     List<String> getExistCat(@Param("name") String name, @Param("orgId") Long orgId);
-    @Query("select x.productCatIds from UnitOfMeasure x where x.name=:name and x.orgId=:orgId and x.id <> : id ")
+    @Query("select x.productCatIds from UnitOfMeasure x where x.name=:name and x.orgId=:orgId and x.id <> :id ")
     List<String> getExistCatExceptId(@Param("name") String name, @Param("orgId") Long orgId,  @Param("id")  Long id);
 }
