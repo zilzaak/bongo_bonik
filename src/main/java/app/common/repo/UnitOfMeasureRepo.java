85f6521093628de836bc0e0a94f84a5f25dbd41c
zilzaak <orchidpust@gmail.com>
2025-05-27 09:55:56 +0600

    Added  util classfnhfghcvbh

--- src/main/java/app/common/repo/UnitOfMeasureRepo.java
+++ src/main/java/app/common/repo/UnitOfMeasureRepo.java
@@ -2,6 +2,8 @@ package app.common.repo;
 
 import app.common.entity.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,13 @@ public interface UnitOfMeasureRepo extends JpaRepository<UnitOfMeasure,Long> {
 
     boolean existsByNameAndOrgIdAndIdNotIn(String name, Long orgId, List<Long> list);
 
-    boolean existsByNameAndOrgId(String name, Long orgId);
+    @Query("select x.productCatIds from  UnitOfMeasure x where x.name=:name and x.orgId=:org ")
+    List<String> getExistCat(@Param("name") String name,
+                             @Param("org") Long org);
+
+    @Query("select x.productCatIds from  UnitOfMeasure x where x.name=:name and x.orgId=:org and " +
+            " x.id <> : id ")
+    List<String> getExistCatExceptId(@Param("name") String name,
+                                     @Param("org") Long org,
+                                     @Param("id") Long id);
 }
