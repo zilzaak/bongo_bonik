f65e665f80e634adadea9050e31f024d5e8a0126
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-06-03 12:00:35 +0600

    --
    --

---
+++ src/main/java/app/modules/supplier/controller/SupplierController.java
@@ -0,0 +1,56 @@
+package app.modules.supplier.controller;
+
+import app.common.dto.MsgResponse;
+import app.common.dto.ProductDTO;
+import app.modules.purchase.service.PurchaseService;
+import app.modules.supplier.service.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+    @Autowired
+    private SupplierService supplierService;
+
+    @PostMapping("/create")
+    ResponseEntity<?> create(@RequestBody ProductDTO dto)
+            throws RuntimeException{
+        if(dto==null){
+            throw new RuntimeException("null data exist in form");
+        }
+        MsgResponse response = supplierService.create(dto);
+        return new ResponseEntity<>(response , HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    ResponseEntity<?> update(@RequestBody ProductDTO dto)
+            throws RuntimeException{
+        if(dto==null){
+            throw new RuntimeException("null data exist in form");
+        }
+        MsgResponse response = supplierService.edit(dto);
+        return new ResponseEntity<>(response , HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/delete")
+    ResponseEntity<?> delete(@RequestBody ProductDTO dto)
+            throws RuntimeException{
+        MsgResponse response = new MsgResponse();
+        return new ResponseEntity<>(response ,HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    ResponseEntity<?> getList(@RequestParam Map<String,String> params)
+            throws RuntimeException{
+        MsgResponse response = new MsgResponse();
+        return new ResponseEntity<>(response ,HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/app/modules/supplier/service/SupplierService.java
+++ src/main/java/app/modules/supplier/service/SupplierService.java
@@ -1,9 +1,24 @@
 package app.modules.supplier.service;
 
+import app.common.dto.MsgResponse;
+import app.common.dto.ProductDTO;
+import app.modules.supplier.repo.SupplierRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SupplierService {
 
+    @Autowired
+    private SupplierRepo supplierRepo;
 
+    public MsgResponse create(ProductDTO dto) {
+
+      return null;
+    }
+
+    public MsgResponse edit(ProductDTO dto) {
+
+        return null;
+    }
 }
