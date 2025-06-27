50b7e0fe7827c5c3fac7b7f2b8231c3afa176ab7
zilzaak <orchidpust@gmail.com>
2025-06-27 11:50:53 +0600

    Added  util classfnhfghcvbh

--- src/main/java/app/common/repo/ProductRepo.java
+++ src/main/java/app/common/repo/ProductRepo.java
@@ -19,9 +19,23 @@ public interface ProductRepo extends JpaRepository<Product,Long> {
     @Query("select p.name from Product p where p.id=:pid ")
     String getProductName(@Param("pid") Long pid);
 
-    @Query("select p from Product p  " +
+    @Query("select p.id as id , " +
+            " p.name as productName , " +
+            " brand.name as brandName , " +
+            " model.name as modelName , " +
+            " cat.name as categoryName ," +
+            " mdwth.name as madeWithName , " +
+            " size.name as sizeName , " +
+            " color.name as colorName , " +
+            " org.name as orgName   " +
+            " from Product p " +
+            " inner join Organization org on org.id=p.orgId   " +
             " left join p.brand brand " +
             " left join p.model model " +
+            " left join ProductCat cat on p.catId=cat.id " +
+            " left join ProductSize size on p.sizeId = size.id " +
+            " left join ProductColor color on p.colorId=color.id " +
+            " left join MadeWith mdwth on mdwth.id=p.madeWithId  " +
             " where ( ?1 is null or p.id=?1 ) " +
             " and ( ?2 is null or p.orgId= ?2 ) " +
             " and ( ?3 is null or brand.id=?3 )  " +
