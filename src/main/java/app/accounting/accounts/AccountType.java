9a8c3b0a5e63999895e94ee5774853ca07bccffe
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-06-30 13:51:12 +0600

    Create AccountType.java

---
+++ src/main/java/app/accounting/accounts/AccountType.java
@@ -0,0 +1,22 @@
+package app.accounting.accounts;
+
+public enum AccountType {
+
+    ASSETS("ASSETS"),              // CURRENT_ASSET(CASH , BANK , INVENTORY , ACC_RECIEVABLE ),FIXED_ASSET(FURNITURE , VEHICLE, PERSONAL_ASSET)
+    LIABILITIES("LIABILITIES"),    // ACC_PAYABLE , LOANS
+    EQUITY("EQUITY"),              // WITHDRAWAL , CAPITAL
+    REVENUE("REVENUE"),            // SALES , OTHER_INCOME
+    EXPENSE("EXPENSE"),            // RENT , SALARIES , OFFICE_SUPPLY
+    COGS("COGS");                  // COST OF PRODUCT
+
+    private final String type;
+
+    AccountType(String type){
+        this.type=type;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+}
