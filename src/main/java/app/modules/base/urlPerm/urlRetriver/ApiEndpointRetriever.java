81ada9afcd0d35567101007ef7583228ebdbb920
zilzaak <orchidpust@gmail.com>
2025-07-17 23:30:59 +0600

    fghfg

--- src/main/java/app/modules/base/urlPerm/urlRetriver/ApiEndpointRetriever.java
+++ src/main/java/app/modules/base/urlPerm/urlRetriver/ApiEndpointRetriever.java
@@ -125,7 +125,10 @@ public class ApiEndpointRetriever {
                }
         }
 
-        this.apiAgainstModuleRepo.saveAll(menuTree);
+        if(apiAgainstModuleRepo.count()<1){
+            apiAgainstModuleRepo.saveAll(menuTree);
+        }
+
     }
 
 
@@ -154,15 +157,4 @@ public class ApiEndpointRetriever {
             }
          }
 
-
-    public void printTree(List<MenuHierarchy> menuTree){
-        for(MenuHierarchy menu : menuTree){
-                if(menu.getApiPattern()!=null){
-                    System.out.println("menu="+menu.getMenu()+" , api = "+menu.getApiPattern()+" , method = "+menu.getMethodName());
-                }
-                if(menu.getDetails().size()>0){
-                   this.printTree(menu.getDetails());
-                }
-            }
-    }
 }
