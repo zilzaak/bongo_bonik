d43ec6812e53a50130b77fa162985360ca6bec56
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-10-25 10:12:15 +0600

    -
    --

--- src/main/java/app/CorsConfig.java
+++ src/main/java/app/CorsConfig.java
@@ -9,20 +9,14 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/auth/**")
-//                .allowedOrigins("http://localhost:4200")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .exposedHeaders("Authorization") // if you use JWT tokens
-//                .allowCredentials(true); // if sending cookies/tokens
-
         registry.addMapping("/**")  // Apply to all endpoints
                 .allowedOriginPatterns(
                         "http://localhost:4200",
                         "http://localhost:4201",
                         "http://localhost:4202",
                         "http://localhost:4203",
-                        "http://localhost:4204"
+                        "http://localhost:4204",
+                        "http://localhost:8080"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
